a6edb8da627263d1a84201ac0777f76103e94639
PontoZawrik <Psichotroniks@tut.by>
2026-05-02 00:51:30 +0300

    Задание 2

--- lab22_WinForms/MyForm.h
+++ lab22_WinForms/MyForm.h
@@ -34,6 +34,12 @@ namespace lab22WinForms {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Label^ label1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +54,97 @@ namespace lab22WinForms {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(24, 26);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(153, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Показать надпись";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(24, 66);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(153, 23);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Скрыть надпись";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(24, 110);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(165, 23);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"Поменять цвет окна";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(24, 159);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"Закрыть";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(208, 45);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(0, 16);
+			this->label1->TabIndex = 4;
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(414, 259);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		label1->Text = "Кнопка работает.";
+	}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	label1->Text = "";
+}
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (this->BackColor == SystemColors::ButtonFace) {
+		this->BackColor = Color::Red;
+	}
+	else {
+		this->BackColor = SystemColors::ButtonFace;
+	}
+}
+private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->Close();
+}
+};
 }
